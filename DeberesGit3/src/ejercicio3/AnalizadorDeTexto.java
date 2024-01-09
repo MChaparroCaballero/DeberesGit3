920ad247248e1301943c8a542dd1ae9b334122c3
MChaparroCaballero <mchaparrocaballeroa706@linkiafp.online>
2024-01-09 22:17:32 +0100

    intento de la seguna parte las repeticiones

--- DeberesGit3/src/ejercicio3/AnalizadorDeTexto.java
+++ DeberesGit3/src/ejercicio3/AnalizadorDeTexto.java
@@ -15,26 +15,41 @@ public class AnalizadorDeTexto {
 		lector = new BufferedReader(info);
 		String texto = lector.readLine();
 		int contadorpalabras = 0;
-		
+
 		System.out.println(texto);
-		 
+		String[] palabras = texto.split("\\s+");
 
-
-	        System.out.println("La cantidad de palabras en el texto es: " + contadordepalabras(texto));
-	    }
-	
-	  private static int contadordepalabras(String text) {
-	        String[] palabras = text.split("\\s+");
-	        int contadordepalabras = 0;
-	        for (String palabra : palabras) {
-	            // Si el String no está vacío, incrementar el contador
-	            contadordepalabras += (palabra.length() > 0) ? 1 : 0;
-	        }
-	        return contadordepalabras;
-	    }
-	  
-	  
-		
+		System.out.println(
+				"La cantidad de palabras en el texto es: " + contadordepalabras(texto) + frecuenciadepalabras(texto));
 	}
 
+	private static int contadordepalabras(String texto) {
+		String[] palabras = texto.split("\\s+");
+		int contadordepalabras = 0;
+		for (String palabra : palabras) {
+			// Si el String no está vacío, incrementar el contador
+			contadordepalabras += (palabra.length() > 0) ? 1 : 0;
+		}
+		return contadordepalabras;
+	}
 
+	private static int frecuenciadepalabras(String texto) {
+
+		String[] palabras = texto.split("\\s+");
+		palabras = new String[1000000000];
+		int repeticion = 0;
+		for (int i = 0; i < palabras.length; i++) {
+
+			if (palabras[i] == palabras[i]) {
+				repeticion = repeticion + 1;
+			} else {
+				repeticion = repeticion;
+
+			}
+		}
+		
+		return repeticion;
+
+	}
+
+}
